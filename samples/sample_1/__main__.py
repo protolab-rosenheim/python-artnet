f10b022e4dffb6f6275350adc2a4fc58f89e53c3
Michael List <michael.list@stud.fh-rosenheim.de>
2018-09-29 11:58:13 +0200

    Made main for sample_1 more simple

--- samples/sample_1/__main__.py
+++ samples/sample_1/__main__.py
@@ -4,8 +4,6 @@ import sys
 from artnet.artnet_configurator import ArtNetConfigurator
 
 sleep_time = 2
-thread_list = []
-
 sample_folder = os.path.dirname(os.path.realpath(sys.argv[0]))
 
 # Starting ArtNetServer
@@ -13,18 +11,17 @@ artnet_configurator = ArtNetConfigurator()
 artnet_server = artnet_configurator.get_artnet_server(sample_folder + '/artnet_config.yml',
                                                       sample_folder + '/led_mapping.yml')
 artnet_server.start_server()
-thread_list.append(artnet_server.thread)
 
-# Run till every thread has finished
-while thread_list:
-    artnet_server.art_net_nodes[0].illuminate_all('green')
-    print('green')
-    sleep(sleep_time)
+# Run till thread has stopped
+while artnet_server.thread.isAlive():
+    try:
+        artnet_server.art_net_nodes[0].illuminate_all('green')
+        print('green')
+        sleep(sleep_time)
 
-    artnet_server.art_net_nodes[0].illuminate_all('red')
-    print('red')
-    sleep(sleep_time)
+        artnet_server.art_net_nodes[0].illuminate_all('red')
+        print('red')
+        sleep(sleep_time)
 
-    for thread in thread_list:
-        if not thread.isAlive():
-            thread_list.remove(thread)
+    except KeyboardInterrupt:
+        artnet_server.stop_server()
