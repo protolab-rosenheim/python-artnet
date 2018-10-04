7c81eae971957971a9549151e069beb1b5f153be
Michael List <michael.list@stud.fh-rosenheim.de>
2018-10-04 10:59:33 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='python-artnet',
+    version='0.1.0',
+    description='ArtNet lib, proto_lab',
+    long_description=readme,
+    author='proto_lab',
+    author_email=' kontakt@proto-lab.de',
+    url='http://protolab-rosenheim.de/',
+    license=license,
+)
